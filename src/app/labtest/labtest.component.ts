3e74c120d3dc8b330bd97c3c8d8ec7161210fa07
Kim Mangahis <kymangahis@live.mcl.edu.ph>
2024-02-16 14:02:00 +0800

    v2.0 - Update IAO + Radiology Added

--- src/app/labtest/labtest.component.ts
+++ src/app/labtest/labtest.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-labtest',
   standalone: true,
   imports: [],
   templateUrl: './labtest.component.html',
-  styleUrl: './labtest.component.scss'
+  styleUrl: './labtest.component.scss',
 })
 export class LabtestComponent {
-  onSubmit(){
-    alert('Form submitted successfully!');
+  constructor(public router: Router) {}
+  refreshPage() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([this.router.url]);
+    });
+  }
+
+  onSubmit(): void {
+    this.refreshPage();
+    alert('Form has been submitted successfully!');
   }
 }
